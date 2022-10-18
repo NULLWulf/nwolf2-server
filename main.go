b7502c63a2dce37050039d75dd53e84b2f79c535
nullwulf <nwolf2@oswego.edu>
2022-10-18 19:57:28 -0400

    add request handler for nwofl2 status

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"time"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/nwolf2/status", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"system time": time.Now().UTC().Format(time.RFC3339),
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
