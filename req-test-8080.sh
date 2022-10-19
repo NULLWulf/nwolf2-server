531c5487c66e4a2868fc76d8c5fa5ee165c96044
nullwulf <nwolf2@oswego.edu>
2022-10-19 16:53:56 -0400

    de-included pem file

---
+++ req-test-8080.sh
@@ -0,0 +1,10 @@
+echo "GET Request Test Request"
+curl -X GET 0.0.0.0:8080/nwolf2/status/
+echo "POST Request Test Request"
+curl -X POST 0.0.0.0:8080/nwolf2/status/
+echo "DELETE Request Test Request"
+curl -X DELETE 0.0.0.0:8080/nwolf2/status/
+echo "PATCH Request Test Request"
+curl -X PATCH 0.0.0.0:8080/nwolf2/status/
+echo "PUT Request Test Request"
+curl -X PUT 0.0.0.0:8080/nwolf2/status/
