71b342e5ce5124b94d0aa5dd22a0a7dc922ec03a
nullwulf <nwolf2@oswego.edu>
2022-10-19 07:28:12 -0400

    Added output to log message

--- main.go
+++ main.go
@@ -1,39 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/nullwulf/loggly"
 	"net/http"
 	"time"
 )
 
+type RequestLog struct {
+	Method      string
+	SourceIP    string
+	RequestPath string
+	StatusCode  int
+}
+
 func main() {
 	r := gin.Default()
-	status := r.Group("/nwolf2/status")
+	status := r.Group("/nwolf2/status/")
 	{
-		status.GET("/", getSystemDate())
-		status.DELETE("/", invalidRequest())
-		status.POST("/", invalidRequest())
-		status.PATCH("/", invalidRequest())
-		status.PUT("/", invalidRequest())
+		status.GET("/", getSystemDate)
+		status.DELETE("/", invalidRequest)
+		status.POST("/", invalidRequest)
+		status.PATCH("/", invalidRequest)
+		status.PUT("/", invalidRequest)
 	}
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
-func getSystemDate() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"system time": time.Now().UTC().Format(time.RFC3339),
-		})
-	}
+func getSystemDate(c *gin.Context) {
+	populateLog(c, 200)
+	c.IndentedJSON(http.StatusOK, gin.H{
+		"system-time": time.Now().UTC().Format(time.RFC3339),
+	})
 }
 
-func logRquest(i interface{}) {
-
+func invalidRequest(c *gin.Context) {
+	populateLog(c, 405)
+	c.IndentedJSON(http.StatusMethodNotAllowed, gin.H{
+		"message": "405 - Method not allowed",
+	})
 }
 
-func invalidRequest() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.JSON(http.StatusMethodNotAllowed, gin.H{"status": http.StatusMethodNotAllowed})
-	}
+func populateLog(c *gin.Context, status int) {
+	logMessage := RequestLog{c.Request.Method, c.ClientIP(), c.FullPath(), status}
+	fmt.Println(logMessage)
 }
