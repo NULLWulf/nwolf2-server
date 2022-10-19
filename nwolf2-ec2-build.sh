d03f46c7b03c906207b48b65aa856ea22d91c862
nullwulf <nwolf2@oswego.edu>
2022-10-19 16:19:49 -0400

    added build and run scripts

---
+++ nwolf2-ec2-build.sh
@@ -0,0 +1,2 @@
+## Docker build script
+docker build --rm -f=Dockerfile -t nwolf2-server:ec2 .
---
+++ nwolf2-run.sh
@@ -0,0 +1,2 @@
+## Script to launch docker container ##
+docker run -d -p 35670:8080 --env=GIN_MODE=release  nwolf2-server:ec2
