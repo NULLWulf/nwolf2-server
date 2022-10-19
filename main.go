f7258d512b2e07963b8a54d68eab83d2b41b8854
nullwulf <nwolf2@oswego.edu>
2022-10-18 20:41:57 -0400

    added handlers for methods

--- main.go
+++ main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"net/http"
 	"time"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/nwolf2/status", func(c *gin.Context) {
+	status := r.Group("/nwolf2/status")
+	{
+		status.GET("/", getSystemDate())
+		status.DELETE("/", invalidRequest())
+		status.POST("/", invalidRequest())
+		status.PATCH("/", invalidRequest())
+		status.PUT("/", invalidRequest())
+	}
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func getSystemDate() gin.HandlerFunc {
+	return func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"system time": time.Now().UTC().Format(time.RFC3339),
 		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	}
+}
+
+func invalidRequest() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(http.StatusMethodNotAllowed, http.StatusMethodNotAllowed)
+	}
 }
