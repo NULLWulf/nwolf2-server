96b75b850996042deb24409f509ecb8dfd7a74ec
nullwulf <nwolf2@oswego.edu>
2022-10-19 12:24:29 -0400

    updated loc

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/gin-gonic/gin v1.8.1
+	github.com/joho/godotenv v1.4.0
 	github.com/nullwulf/loggly v0.0.0-20220928223524-974e4cfcfdda
 )
 
--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/nullwulf/loggly"
-	_ "github.com/nullwulf/loggly"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -18,17 +19,33 @@ type RequestLog struct {
 }
 
 func main() {
+
+	// Loads Environmental variables into program
+	// e.g AWS, Loggly CMP token.
+	err := godotenv.Load()
+	// If detects an error loading .env file terminates program
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	// Instantiate Loggly Client
 	r := gin.Default()
-	status := r.Group("/nwolf2/status/")
-	{
-		status.GET("/", getSystemDate)
-		status.DELETE("/", invalidRequest)
-		status.POST("/", invalidRequest)
-		status.PATCH("/", invalidRequest)
-		status.PUT("/", invalidRequest)
-	}
+
+	r.GET("/nwolf2/status", getSystemDate)
+	r.DELETE("/nwolf2/status", invalidRequest)
+	r.POST("/nwolf2/status", invalidRequest)
+	r.PATCH("/nwolf2/status", invalidRequest)
+	r.PUT("/nwolf2/status", invalidRequest)
+	r.Any("/nwolf2", notFoundResponse)
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func notFoundResponse(c *gin.Context) {
+	populateLog(c, 405)
+	c.IndentedJSON(http.StatusNotFound, gin.H{
+		"message": "404 - Resource Not found",
+	})
 }
 
 func getSystemDate(c *gin.Context) {
