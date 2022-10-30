a1c205aaecb4f82b6f5abe3e2eb67b0459bf3c03
nullwulf <nwolf2@oswego.edu>
2022-10-30 12:49:50 -0400

    final commit for assign 10

---
+++ req-test-ec2.sh
@@ -0,0 +1,10 @@
+echo "GET Request Test Request"
+curl -X GET 34.207.194.208:35670/nwolf2/status
+echo "POST Request Test Request"
+curl -X POST 34.207.194.208:35670/nwolf2/status
+echo "DELETE Request Test Request"
+curl -X DELETE 34.207.194.208:35670/nwolf2/status
+echo "PATCH Request Test Request"
+curl -X PATCH 34.207.194.208:35670/nwolf2/status
+echo "PUT Request Test Request"
+curl -X PUT 34.207.194.208:35670/nwolf2/status
